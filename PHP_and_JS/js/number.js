64e0cc788453f05e765b21b19fe9cc3d340b99d3
Patrick Chung <patrick.chung13@gmail.com>
2013-07-11 10:59:01 +1000

    updated my number guesser

--- PHP_and_JS/js/number.js
+++ PHP_and_JS/js/number.js
@@ -1,4 +1,5 @@
 var numberToGuess = Math.floor(Math.random()*100);
+var previousDifference = Number.MAX_VALUE;
 
 //Function to help you debug
 function showNumberToGuess() {
@@ -18,5 +19,23 @@ function newGame() {
 }
 
 function guess() {
-	//Write a function which checks if the inputted number matches the number to guess
+	var guessVal = document.getElementById("guess").value;
+	
+	if (guessVal == numberToGuess) {
+		document.getElementById("output").innerHTML = "You guessed right!";
+	} else {
+		var output = "Incorrect! ";
+		
+		var difference = Math.abs(numberToGuess - guessVal);
+		
+		if (difference < previousDifference) {
+			output += "but getting warmer";
+		} else {
+			output += "and getting colder";
+		}
+		
+		previousDifference = difference;
+		document.getElementById("output").innerHTML = output;
+		document.getElementsByTagName("body")[0].style.backgroundColor = "rgba(" + Math.min(255, Math.round(255/difference + 1)) + ",0," + Math.min(255, Math.round(difference/2)) + ",0.5)";
+	}
 }
